0ed531e02b1e004fa620cd4cdddf2a9a19fbcf9f
elise <elise.marie.lau@gmail.com>
2018-12-03 13:20:25 +0100

    initiate firebase and text to speech

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -11,6 +12,15 @@ export class MyApp {
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyD6_ZIzpIqEl29f7k0imsuFTa29o3hwVxM",
+      authDomain: "penguinassistant-90944.firebaseapp.com",
+      databaseURL: "https://penguinassistant-90944.firebaseio.com",
+      projectId: "penguinassistant-90944",
+      storageBucket: "penguinassistant-90944.appspot.com",
+      messagingSenderId: "1072275262211"
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import firebase from 'firebase';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private usr: firebase.database.Reference = firebase.database().ref(`/usr/`);
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private tts: TextToSpeech) {
 
+  }
+
+  newMailNotification(): void {
+  this.tts.speak('Hello World')
+    .then(() => console.log('Success'))
+    .catch((reason: any) => console.log(reason));
   }
 
 }
