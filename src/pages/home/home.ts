cd6b3b2192c625d26045181877497a27b3c8e687
elise <elise.marie.lau@gmail.com>
2018-12-12 17:35:39 +0100

    change database architecture

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,20 +10,27 @@ import { TextToSpeech } from '@ionic-native/text-to-speech';
 })
 export class HomePage {
   userDelivery;
+  emailStack;
 
   constructor(public navCtrl: NavController, private tts: TextToSpeech) { }
   
   deliverMsgToUser(userDelivery): void {
-  this.tts.speak(`${userDelivery.msg} ${userDelivery.cmd}`)
+  this.tts.speak(`${userDelivery.msg}. You can read your emails by saying : ${userDelivery.cmd}`)
     .then(() => console.log('Success'))
     .catch((reason: any) => console.log(reason));
   }
 
   ionViewDidLoad() {
-    let usr: firebase.database.Reference = firebase.database().ref(`/usr/`);
-    usr.on('value', snapshot => {
-      this.userDelivery = snapshot.val();
-      this.deliverMsgToUser(this.userDelivery);
+    let email: firebase.database.Reference = firebase.database().ref(`/users/user1/usr/email/`);
+    email.on('value', snapshot => {
+      this.emailStack = snapshot.val();
+      let length = this.emailStack.length;
+
+      for (let i = 0; i < this.emailStack.length; i++) {
+        console.log(this.emailStack[i]);
+      }
+
+      this.deliverMsgToUser(this.emailStack[length-1]);
     });
   }
 
